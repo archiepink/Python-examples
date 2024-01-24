6e5e584de8c285be1e37e7ba5533931c2bbc13d5
archiepink <156815578+archiepink@users.noreply.github.com>
2024-01-24 17:45:05 +0000

    Add files via upload

---
+++ Basic string manipulation.py
@@ -0,0 +1,7 @@
+dna_sequence = "atcgctagctag"
+uppercase_sequence = dna_sequence.upper()
+replaced_sequence = uppercase_sequence.replace('A', 'T')
+
+print('Original: ', dna_sequence)
+print('Uppercase: ', uppercase_sequence)
+print('Replaced A with T: ', replaced_sequence)
