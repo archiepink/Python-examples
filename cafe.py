567173cc8fc5a90024d29df485934436a02c5dfe
archiepink <156815578+archiepink@users.noreply.github.com>
2024-03-24 09:49:56 +0000

    Cafe stocklist

---
+++ cafe.py
@@ -0,0 +1,39 @@
+# The List 'menu' contains elements from a cafe menu.
+menu = ["Coffee", "Tea", "Cake", "Biscuit"]
+
+# The dictionary 'stock' defines the number of units of each element in 'menu'.
+stock = {'Coffee': 20,
+         'Tea': 15,
+         'Cake': 20,
+         'Biscuit': 100 
+        }
+
+# The dictionary 'price' defines the price of elements in 'menu'.
+price = {'Coffee': 3.50,
+         'Tea': 1.50,
+         'Cake': 4.50,
+         'Biscuit': 1.50
+        }
+
+# Create a variable to store the total value of all elements in 'menu'
+total_stock = 0
+
+# Create a variable to iterate through elements in 'menu' using indexing.
+index = 0
+
+print("Stock list for imaginary cafe: \n")
+
+""" 
+Create a 'for' loop to print the type, units and value of each element in 
+'menu', recalling data from the dictionaries 'price' and 'stock' to calculate 
+values and store the total value of the stock in a variable ('total_stock').
+"""
+for i in menu:
+    item_value = (stock[menu[index]]) * price[menu[index]] 
+    print(f'There is {stock[menu[index]]} units of {menu[index]} worth {item_value} pounds.') 
+    total_stock += item_value
+    index += 1
+
+# Print the total stock value.
+print(f'\nThe total worth of the stock is {total_stock} pounds.')       
+
